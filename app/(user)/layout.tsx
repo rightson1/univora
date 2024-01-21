994088f97ad67c081da2c43929189e96a606754d
rightson <chari.rightson@gmail.com>
2024-01-21 21:57:28 +0300

    Fix storage event handling and comment out email sending code

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -11,22 +11,38 @@ const Layout = ({ children }: IChildren) => {
   const router = useRouter();
 
   useEffect(() => {
-    const handleStorageChange = (e: StorageEvent) => {
-      if (e.key === "school") {
-        if (e.newValue) {
-          router.push(`/${e.newValue}`);
-          setSchool(true);
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("school")) {
+        console.log(localStorage.getItem("school"));
+        setSchool(true);
+      } else {
+        console.log("no school");
+        setSchool(false);
+      }
+      const handleStorageChange = (e: StorageEvent) => {
+        console.log(e.key);
+        if (e.key === "school") {
+          console.log(e.newValue);
+          if (e.newValue) {
+            router.push(`/${e.newValue}`);
+            setSchool(true);
+          } else {
+            setSchool(false);
+          }
         } else {
+          console.log("no school");
           setSchool(false);
         }
-      }
-    };
+      };
 
-    window.addEventListener("storage", handleStorageChange);
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
+      window.addEventListener("storage", handleStorageChange);
+
+      return () => {
+        window.removeEventListener("storage", handleStorageChange);
+      };
+    }
   }, [router]);
+  console.log(school);
   return (
     <div
       className="bg-background  min-h-screen max-w-screen overflow-x-hidden
