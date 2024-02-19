41e2d75b3ddfdd78e7c4179fa4ea1d645c819fdb
rightson <chari.rightson@gmail.com>
2024-02-20 00:33:28 +0300

    Refactor middleware and delete unused files

--- middleware.ts
+++ middleware.ts
@@ -12,22 +12,17 @@ export default async function middleware(req: NextRequest) {
       `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
     );
   hostname = hostname.replace("www.", "");
-  // console.log(hostname);
   const searchParams = req.nextUrl.searchParams.toString();
   const path = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ""
   }`;
   if (path.startsWith("/api")) {
-    const role = req.cookies.get("role")?.value || "";
     NextResponse.rewrite(new URL(`${path}`, req.url));
-
-    // if (path.startsWith("/api/s") && role !== "super_admin") {
-    //   return NextResponse.json({
-    //     message: "You are not authorized to access this route",
-    //     success: false,
-    //   });
-    // }
     return NextResponse.next();
+  }
+  //if www rewrite to root domain
+  if (hostname === `www.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    return NextResponse.rewrite(new URL(`${path}`, req.url));
   }
 
   if (hostname == `admin.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
@@ -55,7 +50,7 @@ export default async function middleware(req: NextRequest) {
 
   let subdomain = hostname.split(".")[0];
   if (subdomain && subdomain !== process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
-    return NextResponse.rewrite(new URL(`/${subdomain}`, req.url));
+    return NextResponse.rewrite(new URL(`/school/${subdomain}`, req.url));
   }
 
   return NextResponse.next();
